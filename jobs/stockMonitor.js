dd703f93cae694ae9f78771bf8566b428f0be088
Bhaswanth Polamarasetti <144129081+bhaswanthP@users.noreply.github.com>
2026-02-08 18:31:07 +0530

    Updated stockMonitor.js just to test cron job scheduler is working or not

--- jobs/stockMonitor.js
+++ jobs/stockMonitor.js
@@ -4,6 +4,7 @@ const { getStockPrice } = require('../services/stockService');
 const { sendEmail } = require('../services/emailService');
 
 cron.schedule('*/1 * * * *', async () => {
+  console.log('cron job scheduler testing');
   const alerts = await StockAlert.find({ status: 'active' });
   if (alerts.length == 0) {
     console.log('No alerts');
@@ -14,21 +15,23 @@ cron.schedule('*/1 * * * *', async () => {
       console.log(`${alert.symbol}: ${price}`);
       
       if (price >= alert.targetPrice) {
-        /*await sendEmail(
+        /*
+        await sendEmail(
           alert.email, 
           `📈 Stock Alert: Action Required for ${alert.symbol}!`, 
           `We hope this message finds you well.\n\nWe are writing to inform you about a significant update regarding one of your monitored stocks.\n\n
-      Stock Symbol: ${alert.symbol}\n
-      Current Price: ${price}\n
-      Target Price: ${alert.targetPrice}\n\n
-      We are thrilled to notify you that the current price of ${alert.symbol} has reached or surpassed your target price of ${alert.targetPrice}. This could be an excellent opportunity to review your portfolio and decide on any potential actions you might wish to take.\n\n
-      As always, it’s important to assess the market conditions and consult with your financial advisor before making any decisions. Our platform is here to assist you in staying updated and making informed choices.\n\n
-      Thank you for trusting us to keep you informed about the stocks that matter to you. If you have any questions or need further assistance, please don’t hesitate to reach out to our support team.\n\n
-      Best regards,\n
-      [Your Company/Service Name]\n
-      [Contact Information]`
+            Stock Symbol: ${alert.symbol}\n
+            Current Price: ${price}\n
+            Target Price: ${alert.targetPrice}\n\n
+            We are thrilled to notify you that the current price of ${alert.symbol} has reached or surpassed your target price of ${alert.targetPrice}. This could be an excellent opportunity to review your portfolio and decide on any potential actions you might wish to take.\n\n
+            As always, it’s important to assess the market conditions and consult with your financial advisor before making any decisions. Our platform is here to assist you in staying updated and making informed choices.\n\n
+            Thank you for trusting us to keep you informed about the stocks that matter to you. If you have any questions or need further assistance, please don’t hesitate to reach out to our support team.\n\n
+            Best regards,\n
+            [Your Company/Service Name]\n
+            [Contact Information]`
         );`
-        //*/alert.status = 'notified';
+        */
+        alert.status = 'notified';
         alert.lastNotifiedAt = new Date();
         console.log(`Mail sent to ${alert.email} regarding ${alert.symbol}`);
         await alert.save();
