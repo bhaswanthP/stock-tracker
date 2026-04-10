f692539768419a2066451b975d1406505765cd5b
Bhaswanth Polamarasetti <144129081+bhaswanthP@users.noreply.github.com>
2026-04-11 01:37:47 +0530

    Modified the email format

--- jobs/stockMonitor.js
+++ jobs/stockMonitor.js
@@ -16,6 +16,7 @@ cron.schedule('*/1 * * * *', async () => {
         console.log(`${alert.symbol}: ${price}`);
         
         if (price >= alert.targetPrice) {
+          /*
           await sendEmail(
             alert.email, 
             `📈 Stock Alert: Action Required for ${alert.symbol}!`, 
@@ -29,6 +30,27 @@ cron.schedule('*/1 * * * *', async () => {
               Best regards,\n
               [Your Company/Service Name]\n
               [Contact Information]`
+          );
+          */
+
+          await sendEmail(
+            alert.email, 
+            `📈 Stock Alert: ${alert.symbol} has reached your target!`, 
+            `Hello,
+            We have an important update on one of your monitored stocks:
+            
+            - Stock Symbol: ${alert.symbol}
+            - Current Price: ${price}
+            - Target Price: ${alert.targetPrice}
+            
+            The current price of ${alert.symbol} has reached or exceeded your target of ${alert.targetPrice}. This may be a good time to review your portfolio and consider next steps.
+            
+            Please assess market conditions carefully and consult your financial advisor before making any decisions.
+            
+            Thank you for trusting us to keep you informed. If you have any questions, our support team is here to help. Please reply back to the same mail.
+            
+            Best Regards,
+            Bhaswanth Polamarasetti`
           );
           alert.status = 'Notified';
           alert.lastNotifiedAt = new Date();
