8259f1df2518a7af9cd4184d0337322360cacaae
Bhaswanth Polamarasetti <144129081+bhaswanthP@users.noreply.github.com>
2026-04-11 02:00:55 +0530

    Added back the new mail format.

--- jobs/stockMonitor.js
+++ jobs/stockMonitor.js
@@ -18,20 +18,6 @@ cron.schedule('*/1 * * * *', async () => {
         if (price >= alert.targetPrice) {
           await sendEmail(
             alert.email, 
-            `📈 Stock Alert: Action Required for ${alert.symbol}!`, 
-            `We hope this message finds you well.\n\nWe are writing to inform you about a significant update regarding one of your monitored stocks.\n\n
-              Stock Symbol: ${alert.symbol}\n
-              Current Price: ${price}\n
-              Target Price: ${alert.targetPrice}\n
-              We are thrilled to notify you that the current price of ${alert.symbol} has reached or surpassed your target price of ${alert.targetPrice}. This could be an excellent opportunity to review your portfolio and decide on any potential actions you might wish to take.
-              As always, it’s important to assess the market conditions and consult with your financial advisor before making any decisions. Our platform is here to assist you in staying updated and making informed choices.
-              Thank you for trusting us to keep you informed about the stocks that matter to you. If you have any questions or need further assistance, please don’t hesitate to reach out to our support team.
-              Best Regards,
-              Bhaswanth Polamarasetti`
-          );
-          /*
-          await sendEmail(
-            alert.email, 
             `📈 Stock Alert: ${alert.symbol} has reached your target!`, 
             `Hello,
             We have an important update on one of your monitored stocks:
@@ -49,7 +35,7 @@ cron.schedule('*/1 * * * *', async () => {
             Best Regards,
             Bhaswanth Polamarasetti`
           );
-          */
+          
           alert.status = 'Notified';
           alert.lastNotifiedAt = new Date();
           console.log(`Mail sent to ${alert.email} regarding ${alert.symbol}`);
