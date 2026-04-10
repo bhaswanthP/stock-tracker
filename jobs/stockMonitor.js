887bdca6a7832903b1b9daedd618bcfd1f07403a
Bhaswanth Polamarasetti <144129081+bhaswanthP@users.noreply.github.com>
2026-04-11 01:55:12 +0530

    Reverting the email format to the old format just to debug why I got 2 mails, one is with old format and the other is with new format.

--- jobs/stockMonitor.js
+++ jobs/stockMonitor.js
@@ -16,23 +16,20 @@ cron.schedule('*/1 * * * *', async () => {
         console.log(`${alert.symbol}: ${price}`);
         
         if (price >= alert.targetPrice) {
-          /*
           await sendEmail(
             alert.email, 
             `📈 Stock Alert: Action Required for ${alert.symbol}!`, 
             `We hope this message finds you well.\n\nWe are writing to inform you about a significant update regarding one of your monitored stocks.\n\n
               Stock Symbol: ${alert.symbol}\n
               Current Price: ${price}\n
-              Target Price: ${alert.targetPrice}\n\n
-              We are thrilled to notify you that the current price of ${alert.symbol} has reached or surpassed your target price of ${alert.targetPrice}. This could be an excellent opportunity to review your portfolio and decide on any potential actions you might wish to take.\n\n
-              As always, it’s important to assess the market conditions and consult with your financial advisor before making any decisions. Our platform is here to assist you in staying updated and making informed choices.\n\n
-              Thank you for trusting us to keep you informed about the stocks that matter to you. If you have any questions or need further assistance, please don’t hesitate to reach out to our support team.\n\n
-              Best regards,\n
-              [Your Company/Service Name]\n
-              [Contact Information]`
+              Target Price: ${alert.targetPrice}\n
+              We are thrilled to notify you that the current price of ${alert.symbol} has reached or surpassed your target price of ${alert.targetPrice}. This could be an excellent opportunity to review your portfolio and decide on any potential actions you might wish to take.
+              As always, it’s important to assess the market conditions and consult with your financial advisor before making any decisions. Our platform is here to assist you in staying updated and making informed choices.
+              Thank you for trusting us to keep you informed about the stocks that matter to you. If you have any questions or need further assistance, please don’t hesitate to reach out to our support team.
+              Best Regards,
+              Bhaswanth Polamarasetti`
           );
-          */
-
+          /*
           await sendEmail(
             alert.email, 
             `📈 Stock Alert: ${alert.symbol} has reached your target!`, 
@@ -52,6 +49,7 @@ cron.schedule('*/1 * * * *', async () => {
             Best Regards,
             Bhaswanth Polamarasetti`
           );
+          */
           alert.status = 'Notified';
           alert.lastNotifiedAt = new Date();
           console.log(`Mail sent to ${alert.email} regarding ${alert.symbol}`);
